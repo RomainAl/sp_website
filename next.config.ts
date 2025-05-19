48816327783ad87cd83a123d7b1994b6e12cf636
Romain Al <romainal@gmail.com>
2025-05-19 19:09:33 +0200

    no basPath for CNAME ?

--- next.config.ts
+++ next.config.ts
@@ -3,7 +3,7 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
   output: "export",
-  basePath: "/sp_website",
+  basePath: "",
   images: {
     unoptimized: true,
   },

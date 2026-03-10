a2b5dabcb75175940751d95e55f8aeba415ec1f7
Romain Al <romainal@gmail.com>
2026-03-10 16:33:36 +0100

    new dates

--- app/contact/page.tsx
+++ app/contact/page.tsx
@@ -1,5 +1,7 @@
 "use client";
 
+import { Button } from "@/components/ui/button";
+import { Dialog, DialogContent, DialogHeader, DialogTitle, DialogTrigger } from "@/components/ui/dialog";
 import { motion } from "motion/react";
 import Image from "next/image";
 
@@ -26,51 +28,12 @@ export default function Home() {
           className="object-cover w-full top-0 left-0 z-0 rounded-t-lg hidden xs:block"
         ></Image>
         <div className="flex flex-col gap-2 w-full portrait:sm:w-2/3 landscape:sm:w-1/2 m-auto">
-          <h2 className="text-primary font-bold">PROCHAINES DATES :</h2>
+          <h2 className="text-primary font-bold">ÉVÈNEMENTS À VENIR :</h2>
           <div className="text-justify flex flex-col gap-2">
             <div>
-              <span className="text-primary/50 font-bold">L&apos;Autre Canal</span>{" "}
-              <span className="text-foreground text-xs font-normal">(SMAC, Nancy)</span>
+              <span className="text-primary font-bold">DN[A]</span> <span className="text-foreground text-xs font-normal">(Grenoble)</span>
               <br />
-              <s>
-                <strong>3 mai 2025</strong>
-              </s>{" "}
-              <span className="text-foreground text-xs font-normal">(sortie de résidence / bêta test)</span>
-            </div>
-            <div>
-              <span className="text-primary/50 font-bold">Césaré</span> <span className="text-foreground text-xs font-normal">(CNCM, Reims)</span>
-              <br />
-              <s>
-                <strong>26 sept. 2025</strong>
-              </s>{" "}
-              <span className="text-foreground text-xs font-normal">(Festival Rêves électroniques)</span>
-            </div>
-            <div>
-              <span className="text-primary/50 font-bold">Bords 2 Scènes</span>{" "}
-              <span className="text-foreground text-xs font-normal">(SMAC, Vitry-le-François)</span>
-              <br />
-              <s>
-                <strong>22 nov. 2025</strong>
-              </s>{" "}
-              <span className="text-foreground text-xs font-normal">(+ actions culturelles)</span>
-            </div>
-            <div>
-              <span className="text-primary/50 font-bold">Théâtre de Vanves</span>{" "}
-              <span className="text-foreground text-xs font-normal">(Scène conventionnée)</span>
-              <br />
-              <s>
-                <strong>05 déc. 2025</strong>
-              </s>{" "}
-              <span className="text-foreground text-xs font-normal">(+ actions culturelles)</span>
-            </div>
-            <div>
-              <span className="text-primary/50 font-bold">Ici l&apos;Onde</span>{" "}
-              <span className="text-foreground text-xs font-normal">(CNCM, Dijon)</span>
-              <br />
-              <s>
-                <strong>30 janv. 2026</strong>
-              </s>{" "}
-              <span className="text-foreground text-xs font-normal">(Festival Souffle)</span>
+              <strong>30 mai 2026</strong> <span className="text-foreground text-xs font-normal">(Festival des Arts Numériques, par ARCAN)</span>
             </div>
             <div>
               <span className="text-primary font-bold">Nuit Blanche</span> <span className="text-foreground text-xs font-normal">(Paris)</span>
@@ -84,8 +47,70 @@ export default function Home() {
               <br />
               <strong>Rentrée 2026</strong> <span className="text-foreground text-xs font-normal">(+ actions culturelles)</span>.
             </div>
+            <div>
+              <Dialog>
+                <DialogTrigger asChild>
+                  <Button variant="biodial" className="p-0 -my-10 portrait:sm:text-2xl text-primary/50">
+                    ╰┈➤ ÉVÈNEMENTS PASSÉS
+                  </Button>
+                </DialogTrigger>
+                <DialogContent className="sm:max-w-[650px]">
+                  <DialogHeader>
+                    <DialogTitle className="text-primary">ÉVÈNEMENTS PASSÉS :</DialogTitle>
+                    <div className="flex flex-col text-justify gap-2 text-sm">
+                      <div>
+                        <span className="text-primary/50 font-bold">L&apos;Autre Canal</span>{" "}
+                        <span className="text-foreground text-xs font-normal">(SMAC, Nancy)</span>
+                        <br />
+                        <s>
+                          <strong>3 mai 2025</strong>
+                        </s>{" "}
+                        <span className="text-foreground text-xs font-normal">(sortie de résidence / bêta test)</span>
+                      </div>
+                      <div>
+                        <span className="text-primary/50 font-bold">Césaré</span>{" "}
+                        <span className="text-foreground text-xs font-normal">(CNCM, Reims)</span>
+                        <br />
+                        <s>
+                          <strong>26 sept. 2025</strong>
+                        </s>{" "}
+                        <span className="text-foreground text-xs font-normal">(Festival Rêves électroniques)</span>
+                      </div>
+                      <div>
+                        <span className="text-primary/50 font-bold">Bords 2 Scènes</span>{" "}
+                        <span className="text-foreground text-xs font-normal">(SMAC, Vitry-le-François)</span>
+                        <br />
+                        <s>
+                          <strong>22 nov. 2025</strong>
+                        </s>{" "}
+                        <span className="text-foreground text-xs font-normal">(+ actions culturelles)</span>
+                      </div>
+                      <div>
+                        <span className="text-primary/50 font-bold">Théâtre de Vanves</span>{" "}
+                        <span className="text-foreground text-xs font-normal">(Scène conventionnée)</span>
+                        <br />
+                        <s>
+                          <strong>05 déc. 2025</strong>
+                        </s>{" "}
+                        <span className="text-foreground text-xs font-normal">(+ actions culturelles)</span>
+                      </div>
+                      <div>
+                        <span className="text-primary/50 font-bold">Ici l&apos;Onde</span>{" "}
+                        <span className="text-foreground text-xs font-normal">(CNCM, Dijon)</span>
+                        <br />
+                        <s>
+                          <strong>30 janv. 2026</strong>
+                        </s>{" "}
+                        <span className="text-foreground text-xs font-normal">(Festival Souffle)</span>
+                      </div>
+                    </div>
+                  </DialogHeader>
+                </DialogContent>
+              </Dialog>
+            </div>
           </div>
         </div>
+
         <div className="flex flex-col gap-2 w-full portrait:sm:w-2/3 landscape:sm:w-1/2 m-auto">
           <h2 className="text-primary font-bold">CONTACT :</h2>
           <p className="font-bold">
